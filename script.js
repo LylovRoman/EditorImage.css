78e0d2ac89a6b3789562ecffc6467d039c7ae8c2
LylovRoman <roman.lylov@mail.ru>
2022-10-28 13:08:20 +0500

    add settings

--- script.js
+++ script.js
@@ -1,6 +1,4 @@
-filterOptions = document.querySelectorAll(".filter button"),
-filterName = document.querySelector(".filter-info .name"),
-filterValue = document.querySelector(".filter-info .value"),
+filterOptions = document.querySelectorAll(".filter .filter-img"),
 filterSlider = document.querySelector(".slider input"),
 volumeSlider = document.querySelector("#volume"),
 rotateOptions = document.querySelectorAll(".rotate button"),
@@ -11,18 +9,28 @@ offMusic = document.querySelector("#off-music"),
 saveImgBtn = document.querySelector(".save-img"),
 loadImgBtns = document.querySelectorAll(".load"),
 setValues = document.querySelector("#setValues"),
+resetValues = document.querySelector("#resetValues"),
 setkaWidth = document.querySelector("#width"),
 setkaHeight= document.querySelector("#height"),
+setkaSize= document.querySelector("#size"),
 table = document.querySelector(".preview-img table"),
-zIndex = 1;
+slot = document.querySelector("#slot"),
+fonch = document.querySelector("#fonch"),
+zIndex = 21;
 
 let brightness = "100", saturation = "100", inversion = "0", grayscale = "0";
 let rotate = 0, flipHorizontal = 1, flipVertical = 1, moveX = 0, moveY = 0;
 
-const loadImage = (image = previewImg, createOnDesk = true) => {
+const loadImage = (image = previewImg, createOnDesk = true, deleted = true) => {
     let newImage = new Image();
     newImage.src = image.src;
-    newImage.style = "position: absolute; z-index:" + zIndex++ + ";";
+    slot.src = image.src;
+    if (deleted){
+      newImage.style = "position: absolute; z-index:" + zIndex++ + ";";
+    } else {
+      newImage.style = "pointer-events: none; position: absolute; z-index:" + zIndex++ + ";";
+    }
+    newImage.width = newImage.width/8 * setkaSize.value;
     imgParent.prepend(newImage);
 
     previewImg = document.querySelector(".preview-img img");
@@ -33,21 +41,32 @@ const loadImage = (image = previewImg, createOnDesk = true) => {
       newImage.remove();
     }
 
-    previewImg.addEventListener("mouseup", () => {
+    if (deleted){
+      previewImg.addEventListener("mouseup", () => {
         newImage.remove();
-    });
-    previewImg.addEventListener("mousemove", (e) => {
-      if (e.which == 1){
-        newImage.remove();
-      }
-    });
+      });
+      previewImg.addEventListener("mousemove", (e) => {
+        if (e.which == 1){
+          newImage.remove();
+        }
+      });
+    }
 }
+let buttons = document.querySelectorAll(".hide-self");
+buttons.forEach(button => {
+  button.addEventListener("click", () => {
+    button.classList.add("hide");
+  })
+});
 document.querySelectorAll(".btn").forEach(button => {
   let active = null;
-  if (button.id == "block" || button.id == "item" || button.id == "interface" || button.id == "creatures" || button.id == "settings"){
+  if (button.id == "block" || button.id == "item" || button.id == "human" || button.id == "creatures" || button.id == "settings"){
     button.addEventListener("click", () => {
-      active = document.querySelector("#popup_" + button.id);
-      active.classList.add("visible");
+      buttons.forEach(but => {
+        but.classList.add("hide");
+      })
+      active = document.querySelector("#" + button.id + "-div");
+      active.classList.remove("hide");
     })
   }
   if (button.id == "music"){
@@ -96,7 +115,7 @@ document.querySelectorAll(".popup_close").forEach(popupClose => {
 
 setka.addEventListener('click', () => {
   table.classList.toggle("visible");
-  table.setAttribute("style", `z-index: 999999;`);
+  table.setAttribute("style", `z-index: 9000;`);
 });
 loadImgBtns.forEach(load => {
   load.addEventListener('click', () => {
@@ -115,36 +134,28 @@ filterOptions.forEach(option => {
   option.addEventListener('click', () => {
     document.querySelector('.active').classList.remove('active');
     option.classList.add('active');
-    filterName.innerText = option.innerText;
 
     if(option.id === 'brightness') {
       filterSlider.max = '200';
       filterSlider.value = brightness;
-      filterValue.innerText = `${brightness}%`;
     } else if(option.id === 'saturation') {
       filterSlider.max = '200';
       filterSlider.value = saturation;
-      filterValue.innerText = `${saturation}%`
     } else if(option.id === 'inversion') {
       filterSlider.max = '100';
       filterSlider.value = inversion;
-      filterValue.innerText = `${inversion}%`;
     } else if (option.id === 'grayscale'){
       filterSlider.max = '100';
       filterSlider.value = grayscale;
-      filterValue.innerText = `${grayscale}%`;
     } else {
       filterSlider.max = '360';
       filterSlider.value = rotate;
-      filterValue.innerText = `${rotate}%`;
     }
   });
 });
 
 const updateFilter = () => {
-  filterValue.innerText = `${filterSlider.value}%`;
   const selectedFilter = document.querySelector('.filter .active');
-
   if(selectedFilter.id === 'brightness') {
     brightness = filterSlider.value;
   } else if(selectedFilter.id === 'saturation') {
@@ -164,48 +175,48 @@ document.addEventListener('keydown', (event) => {
       if (event.ctrlKey) {
         moveX += 1;
       } else if (event.shiftKey){
-        moveX += 16;
+        moveX += setkaSize.value * 2;
       } else if (event.altKey){
-        moveX += 32;
+        moveX += setkaSize.value * 4;
         loadImage();
       } else {
-        moveX += 32;
+        moveX += setkaSize.value * 4;
       }
       break;
     case 'ArrowLeft':
       if (event.ctrlKey) {
         moveX -= 1;
       } else if (event.shiftKey){
-        moveX -= 16;
+        moveX -= setkaSize.value * 2;
       } else if (event.altKey){
-        moveX -= 32;
+        moveX -= setkaSize.value * 4;
         loadImage();
       } else {
-        moveX -= 32;
+        moveX -= setkaSize.value * 4;
       }
       break;
     case 'ArrowUp':
       if (event.ctrlKey) {
         moveY -= 1;
       } else if (event.shiftKey){
-        moveY -= 16;
+        moveY -= setkaSize.value * 2;
       } else if (event.altKey){
-        moveY -= 32;
+        moveY -= setkaSize.value * 4;
         loadImage();
       } else {
-        moveY -= 32;
+        moveY -= setkaSize.value * 4;
       }
       break;
     case 'ArrowDown':
       if (event.ctrlKey) {
         moveY += 1;
       } else if (event.shiftKey){
-        moveY += 16;
+        moveY += setkaSize.value * 2;
       } else if (event.altKey){
-        moveY += 32;
+        moveY += setkaSize.value * 4;
         loadImage();
       } else {
-        moveY += 32;
+        moveY += setkaSize.value * 4;
       }
       break;
     case 'к':
@@ -224,45 +235,45 @@ rotateOptions.forEach(option => {
       if (event.ctrlKey) {
         moveX -= 1;
       } else if (event.shiftKey){
-        moveX -= 16;
+        moveX -= setkaSize.value * 2;
       } else if (event.altKey){
-        moveX -= 32;
+        moveX -= setkaSize.value * 4;
         loadImage();
       } else {
-        moveX -= 32;
+        moveX -= setkaSize.value * 4;
       }
     } else if(option.id === 'right') {
       if (event.ctrlKey) {
         moveX += 1;
       } else if (event.shiftKey){
-        moveX += 16;
+        moveX += setkaSize.value * 2;
       } else if (event.altKey){
         moveX += previewImg.naturalWidth;
         loadImage();
       } else {
-        moveX += 32;
+        moveX += setkaSize.value * 4;
       }
     } else if(option.id === 'up') {
       if (event.ctrlKey) {
         moveY -= 1;
       } else if (event.shiftKey){
-        moveY -= 16;
+        moveY -= setkaSize.value * 2;
       } else if (event.altKey){
-        moveY -= 32;
+        moveY -= setkaSize.value * 4;
         loadImage();
       } else {
-        moveY -= 32;
+        moveY -= setkaSize.value * 4;
       }
     } else if (option.id === 'down'){
       if (event.ctrlKey) {
         moveY += 1;
       } else if (event.shiftKey){
-        moveY += 16;
+        moveY += setkaSize.value * 2;
       } else if (event.altKey){
         moveY += previewImg.naturalHeight;
         loadImage();
       } else {
-        moveY += 32;
+        moveY += setkaSize.value * 4;
       }
     } else if (option.id === 'horizontal') {
       flipHorizontal = flipHorizontal === 1 ? -1 : 1;
@@ -303,19 +314,22 @@ const saveImage = () => {
   };
 
 function createTable(x, y){
+  imgParent.setAttribute("style", `min-width: ${y * (setkaSize.value * 4) + 2}px; min-height: ${x * (setkaSize.value * 4) + 2}px;`);
+  document.querySelector('#lenta').setAttribute("style", `position: absolute; top: 0px; left: 0px; height: 50px; margin: 0px; z-index: 1; width: ${y * (setkaSize.value * 4) + 303}px; min-width: 100%`);
+  document.querySelector('#polosa').setAttribute("style", `position: absolute; top: 0px; left: 0px; height: ${x * (setkaSize.value * 4) + 200}px; width: 205px; min-height: 100%`);
   x++;
   y++;
-  imgParent.setAttribute("style", `width: ${y * 32}px; height: ${x * 32}px;`);
   let count = 0;
   for (let i = 1; i < x; i++){
     let tr = document.createElement('tr');
     for (let j = 1; j < y; j++){
       let td = document.createElement('td');
+      td.setAttribute("style", `width: ${setkaSize.value * 4 - 2}px; height: ${setkaSize.value * 4 - 2}px; min-width: ${setkaSize.value * 4 - 2}px; min-height: ${setkaSize.value * 4 - 2}px; border: black solid 1px;`)
       td.data = {
         x: i,
         y: j,
-        xBlock: (i - (x / 2)) * 32,
-        yBlock: (j - (y / 2)) * 32,
+        xBlock: (i - (x / 2)) * (setkaSize.value * 4),
+        yBlock: (j - (y / 2)) * (setkaSize.value * 4),
       }
       td.addEventListener('mousedown', (e) => {
         if (e.which == 1) {
@@ -323,7 +337,7 @@ function createTable(x, y){
         }
       });
       imgParent.addEventListener('mouseup', (e) => {
-        table.setAttribute("style", `z-index: 999999;`);
+        table.setAttribute("style", `z-index: 9000;`);
       });
       td.addEventListener('contextmenu', (e) => {
         e.preventDefault();
@@ -349,11 +363,26 @@ function createTable(x, y){
 }
 setkaWidth.value = localStorage.getItem("setkaWidth");
 setkaHeight.value = localStorage.getItem("setkaHeight");
+setkaSize.value = localStorage.getItem("setkaSize");
 if (!setkaWidth.value){
-  setkaWidth.value = 32;
+  setkaWidth.value = 50;
 }
 if (!setkaHeight.value) {
-  setkaHeight.value = 20;
+  setkaHeight.value = 32;
+}
+if (!setkaSize.value) {
+  setkaSize.value = 8;
+}
+if (setkaWidth.value != 50 || setkaHeight.value != 32){
+  document.querySelectorAll(".preview-img img").forEach(image => {
+    image.remove();
+  });
+} else if (setkaSize.value != 8){
+  document.querySelectorAll(".preview-img img").forEach(image => {
+    image.remove();
+  });
+  loadImage(fonch, true, false);
+  previewImg = undefined;
 }
 setValues.addEventListener("click", () => {
   if (setkaWidth.value){
@@ -362,8 +391,30 @@ setValues.addEventListener("click", () => {
   if (setkaHeight.value){
     localStorage.setItem("setkaHeight", setkaHeight.value);
   }
+  if (setkaSize.value) {
+    localStorage.setItem("setkaSize", setkaSize.value);
+  }
   location.reload();
 });
+resetValues.addEventListener("click", () => {
+  if (setkaWidth.value){
+    localStorage.setItem("setkaWidth", 50);
+  }
+  if (setkaHeight.value){
+    localStorage.setItem("setkaHeight", 32);
+  }
+  if (setkaSize.value){
+    localStorage.setItem("setkaSize", 8);
+  }
+  location.reload();
+});
+
+document.querySelectorAll(".popup_caller").forEach(call => {
+  call.addEventListener("click", () => {
+    document.querySelector("#popup_" + call.id).classList.add("visible");
+  });
+});
+
 
 createTable(setkaHeight.value, setkaWidth.value);
 filterSlider.addEventListener('input', updateFilter);
